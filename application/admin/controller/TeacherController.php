d8e36b8cffb728b2f20404abe2cd56b995eccb5b
zhangwenda4917 <l>
2019-09-19 20:11:13 +0800

    课表

--- application/admin/controller/TeacherController.php
+++ application/admin/controller/TeacherController.php
@@ -1,6 +1,10 @@
 <?php
 namespace app\admin\controller;
 use app\common\model\Teacher;  // 教师模型
+use app\common\model\TeacherCourse;
+use app\common\model\CourseList;
+use app\common\model\Klass;
+use app\common\model\Term;
 use think\facade\Request;   
 use think\Controller;   // 请求
 /**
@@ -8,115 +12,125 @@ use think\Controller;   // 请求
  */
 class TeacherController extends Controller
 {
-   protected $batchValidate = true;
-    public function index()
-    {
+ protected $batchValidate = true;
+ public function index()
+ {
         // 获取查询信息
-        $name = Request::instance()->get('name');
+    $name = Request::instance()->get('name');
         // 实例化Teacher
-        $Teacher = new Teacher; 
-        trace($Teacher, 'debug');
+    $Teacher = new Teacher; 
+    trace($Teacher, 'debug');
         // 按条件查询数据并调用分页
-        $teachers = Teacher::where('name', 'like', '%' . $name . '%')->paginate(5); 
+    $teachers = Teacher::where('name', 'like', '%' . $name . '%')->paginate(5); 
         // 向V层传数据
-        $this->assign('teachers', $teachers);
+    $this->assign('teachers', $teachers);
         // 取回打包后的数据
-        $htmls = $this->fetch();
+    $htmls = $this->fetch();
         // 将数据返回给用户
-        return $htmls;
-    }
-    public function add()
-    {
+    return $htmls;
+}
+public function add()
+{
+    $terms = Term::all();
+    $klass = Klass::all();
         // 实例化
-        $Teacher = new Teacher;
+    $Teacher = new Teacher;
         // 设置默认值
-        $Teacher->id = 0;
-        $Teacher->name = '';
-        $Teacher->username = '';
-        $Teacher->sex = 0;
-        $Teacher->email = '';
-        $this->assign('Teacher', $Teacher);
+    $Teacher->id = 0;
+    $Teacher->name = '';
+    $Teacher->term_id = 0;
+    $Teacher->username = '';
+    $Teacher->sex = 0;
+    $Teacher->email = '';
+    $this->assign('terms',$terms);
+    $this->assign('klass',$klass);
+    $this->assign('Teacher', $Teacher);
         // 调用edit模板
-        return $this->fetch('edit');
-    }
-    public function delete()
-    {
-        try {
+    return $this->fetch('edit');
+}
+public function delete()
+{
+    try {
             // 获取get数据
-            $Request = Request::instance();
-            $id = Request::instance()->param('id/d');
-            
+        $Request = Request::instance();
+        $id = Request::instance()->param('id/d');
+        
             // 判断是否成功接收
-            if (is_null($id) || 0 === $id) {
-                throw new \Exception('未获取到ID信息', 1);
-            }
+        if (is_null($id) || 0 === $id) {
+            throw new \Exception('未获取到ID信息', 1);
+        }
             // 获取要删除的对象
-            $Teacher = Teacher::get($id);
-            // 要删除的对象存在
-            if (is_null($Teacher)) {
-                throw new \Exception('不存在id为' . $id . '的教师，删除失败', 1);
-            }
-            // 删除对象
-            if (!$Teacher->delete()) {
-                return $this->error('删除失败:' . $Teacher->getError());
-            }
-        // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
-        } catch (\think\Exception\HttpResponseException $e) {
-            throw $e;
-        // 获取到正常的异常时，输出异常
-        } catch (\Exception $e) {
-            return $e->getMessage();
-        } 
-        // 进行跳转
-        return $this->success('删除成功', $Request->header('referer')); 
-    }
-    public function edit()
-    {
-        try {
-            // 获取传入ID
-            $id = Request::instance()->param('id/d');
-            // 在Teacher表模型中获取当前记录
-            if (null === $Teacher = Teacher::get($id))
-            {
-                // 由于在$this->error抛出了异常，所以也可以省略return(不推荐)
-                $this->error('系统未找到ID为' . $id . '的记录');
-            } 
-            
-            // 将数据传给V层
-            $this->assign('Teacher', $Teacher);
-            // 获取封装好的V层内容
-            $htmls = $this->fetch();
-            // 将封装好的V层内容返回给用户
-            return $htmls;
-        // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
-        } catch (\think\Exception\HttpResponseException $e) {
-            throw $e;
-        // 获取到正常的异常时，输出异常
-        } catch (\Exception $e) {
-            return $e->getMessage();
-        } 
-    }
-    public function update()
-    {
-     
-        // 接收数据，取要更新的关键字信息
-        $id = Request::instance()->post('id/d');
-        // 获取当前对象
         $Teacher = Teacher::get($id);
-        if (!is_null($Teacher)) {
-            $result = $this->validate(
-      [
-   'name' => Request::instance()->post('name'),
-   'sex' => Request::instance()->post('sex'),
-    'email' => Request::instance()->post('email'),
-      ],
-      'app\admin\validate\Teacher.edit');
-          if ( true !== $result) 
-          {
+            // 要删除的对象存在
+        if (is_null($Teacher)) {
+            throw new \Exception('不存在id为' . $id . '的教师，删除失败', 1);
+        }
+            // 删除对象
+        if (!$Teacher->delete()) {
+            return $this->error('删除失败:' . $Teacher->getError());
+        }
+        // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
+    } catch (\think\Exception\HttpResponseException $e) {
+        throw $e;
+        // 获取到正常的异常时，输出异常
+    } catch (\Exception $e) {
+        return $e->getMessage();
+    } 
+        // 进行跳转
+    return $this->success('删除成功', $Request->header('referer')); 
+}
+public function edit()
+{
+
+    try {
+         $terms = Term::all();
+        $klass = Klass::all();
+            // 获取传入ID
+        $id = Request::instance()->param('id/d');
+            // 在Teacher表模型中获取当前记录
+        if (null === $Teacher = Teacher::get($id))
+        {
+                // 由于在$this->error抛出了异常，所以也可以省略return(不推荐)
+            $this->error('系统未找到ID为' . $id . '的记录');
+        } 
+
+            // 将数据传给V层
+        $this->assign('klass',$klass);
+        $this->assign('terms',$terms);
+        $this->assign('Teacher', $Teacher);
+            // 获取封装好的V层内容
+        $htmls = $this->fetch();
+            // 将封装好的V层内容返回给用户
+        return $htmls;
+        // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
+    } catch (\think\Exception\HttpResponseException $e) {
+        throw $e;
+        // 获取到正常的异常时，输出异常
+    } catch (\Exception $e) {
+        return $e->getMessage();
+    } 
+}
+public function update()
+{
+   
+        // 接收数据，取要更新的关键字信息
+    $id = Request::instance()->post('id/d');
+        // 获取当前对象
+    $Teacher = Teacher::get($id);
+    if (!is_null($Teacher)) {
+        $result = $this->validate(
+          [
+             'name' => Request::instance()->post('name'),
+             'sex' => Request::instance()->post('sex'),
+             'email' => Request::instance()->post('email'),
+         ],
+         'app\admin\validate\Teacher.edit');
+        if ( true !== $result) 
+        {
             dump($result);
-          } 
-           
-       else {
+        } 
+        
+        else {
             // 接收传入数据
             $postData = Request::instance()->post();    
             // 为对象赋值
@@ -124,44 +138,63 @@ class TeacherController extends Controller
             $Teacher->username = $Teacher->username;
             $Teacher->sex = $postData['sex'];
             $Teacher->email = $postData['email'];
-           $Teacher->force()->save();
+            $Teacher->force()->save();
             // 成功跳转至index触发器
-        return $this->success('操作成功', url('index'));
-        }
-    
-       }
-    }
- 
-    public function save()
-    {
-        // 实例化
-        $Teacher = new Teacher;
-        $result = $this->validate(
-      [
-    'name' => Request::instance()->post('name'),
-    'username' => Request::instance()->post('username'),
-     'sex' => Request::instance()->post('sex'),
-    'email' => Request::instance()->post('email'),
-      ],
-      'app\admin\validate\Teacher.add');
-      if ( true !== $result) 
-          {
-            dump($result);
-          } 
-          else {
-             // 接收传入数据
-            $postData = Request::instance()->post();    
-            // 实例化Teacher空对象
-            $Teacher = new Teacher();
-            // 为对象赋值
-            $Teacher->name = $postData['name'];
-            $Teacher->username = $postData['username'];
-            $Teacher->sex = $postData['sex'];
-            $Teacher->email = $postData['email'];
-            // 新增对象至数据表
-            $saves = $Teacher->save();
-            return $this->success('操作成功', url('index'));
-           
-          }
+            $map = ['teacher_id'=>$id];
+            $klassIds = Request::instance()->post('Klass_id/a'); 
+            if (false === $Teacher->TeacherKlasses()->where($map)->delete()) {
+                return $this->error('删除教师课程关联信息发生错误' . $Teacher->TeacherKlasses()->getError());
+            }
+            if (!is_null($klassIds)) {
+                if (!$Teacher->Klasses()->saveAll($klassIds)) {
+                    return $this->error('课程-班级信息保存错误：' . $Teacher->Klasses()->getError());
+                }
             } 
-          }        
+            unset($TeacherKlass);
+            return $this->success('操作成功', url('index'));
+        }
+        
+    }
+}
+
+public function save()
+{
+        // 实例化
+    $Teacher = new Teacher;
+    $result = $this->validate(
+      [
+        'name' => Request::instance()->post('name'),
+        'username' => Request::instance()->post('username'),
+        'sex' => Request::instance()->post('sex'),
+        'email' => Request::instance()->post('email'),
+    ],
+    'app\admin\validate\Teacher.add');
+    if ( true !== $result) 
+    {
+        dump($result);
+    } 
+    else {
+             // 接收传入数据
+        $postData = Request::instance()->post();    
+            // 实例化Teacher空对象
+        $Teacher = new Teacher();
+            // 为对象赋值
+        $Teacher->name = $postData['name'];
+        $Teacher->username = $postData['username'];
+        $Teacher->sex = $postData['sex'];
+        $Teacher->email = $postData['email'];
+        $Teacher->term_id = $postData['term_id'];
+            // 新增对象至数据表
+        $Teacher->save();
+        $klassIds = Request::instance()->post('Klass_id/a'); 
+        dump($klassIds); 
+        if (!is_null($klassIds)) {
+            if (!$Teacher->Klasses()->saveAll($klassIds)) {
+                return $this->error('课程-班级信息保存错误：' . $Teacher->Klasses()->getError());
+            }
+        }
+        return $this->success('操作成功', url('index'));
+        
+    }
+} 
+}        
--- application/index/controller/StudentController.php
+++ application/index/controller/StudentController.php
@@ -1,10 +1,16 @@
 <?php
-namespace app\index\Controller;
-use app\common\model\Student;  // 教师模型
-use think\facade\Request;   
+namespace app\index\controller;
+use app\common\model\Student;  // 学生模型
+use app\common\model\Teacher;
+use app\common\model\Score;
+use app\common\model\Course;
+use app\common\model\CourseList;
+use app\common\model\Term;
+use think\facade\Request; 
+use app\common\model\Chat;
 use think\Controller;   // 请求
 
-class StudentController extends IndexController
+class StudentController extends StudentIndexController
 {
 	protected $batchValidate = true;
 	public function index()
@@ -14,53 +20,209 @@ class StudentController extends IndexController
 
         // 将数据返回给用户
         return $htmls;
-	}
+    }
 	//查看当天的课表
-	public function courseList()
-	
-	{
+    public function courseList()
+    {
+        //获取当前登陆学生的id
+        $id = session('studentId'); 
+        //获取课表的所有信息
+        $list = CourseList::all();
+        //开学时间
+        $date = '2019-08-26';
+        //开学当天是周几 
+        $w    = date('w',strtotime($date));//0代表周日  0-6 日-六
+        
+        //开学周的周一的日期
+        $kx_week = date('Y-m-d',strtotime("$date -".($w ? $w - 1 : 6).' days'));//第一周周1日期
+        
+        //当前日期
+        $date = date('Y-m-d'); 
+        
+        //当前是周几
+        $day    = date('w',strtotime($date));
+        
+        //当前周次的周一的日期
+        $current_week = date('Y-m-d',strtotime("$date -".($day ? $day - 1 : 6).' days'));
+        //当前周次  
+        $week = (strtotime($current_week) - strtotime($kx_week))/(3600*24*7) + 1;
+        //获取与当前登录学生相关的信息，并筛选出班级的id
+        $klassId = Student::where('id',$id)->column('klass_id');
+        //根据班级的id和年月日信息找到当天的课程信息
+        $courseList = CourseList::where('klass_id',$klassId[0])->where('week_id',$week)->where('date_id',$day)->select();
+        if(!($courseList->count() === 0))
+        {
+            foreach ($courseList as $key => $value)
+            {
+         // $value即为courseList的id  用map这个数组承接
+                $map['id'] = $value["id"];
+         //用键值$key区分courseList对象  用get()方法获得id，从而获得courseList对象
+                $course[$key] = CourseList::get($map);
+            }
+        } else {
+            
+            $course = 0;
+        }
+        $this->assign('course',$course);
 		 // 取回打包后的数据
         $htmls = $this->fetch();
 
         // 将数据返回给用户
         return $htmls;
-	}
+    }
 	//扫码登录
-	public function signin()
-	{
+    public function signin()
+    {
 		 // 取回打包后的数据
         $htmls = $this->fetch();
 
         // 将数据返回给用户
         return $htmls;
-	}
+    }
 	//查询分数
-	public function getScore()
-	{
+    public function getScore()
+    {
+		//获取当前登陆学生的id
+        $id = session('studentId'); 
+        //获取score表里的全部信息
+        $score = Score::all();
+        //获取与当前学生id相同的id的学生的全部信息，筛选出term_id字段
+        $Termsid = Score::where('student_id',$id)->column('term_id');
+        //对term_id这个字段进行筛选
+        $TermId = array_unique($Termsid);
+        //根据term_id这个字段进行逐个循环，根据id获取Term这个对象，以便得到term的name
+        foreach ($TermId as $key => $value)
+        {
+         // $value即为term的id  用map这个数组承接
+            $map['id'] = $value;
+         //用键值$key区分term对象  用get()方法获得id，从而获得term对象
+            $term[$key] = Term::get($map);
+        } 
+           //将获得的对象数组传到v层             
+        $this->assign('term',$term);
 		 // 取回打包后的数据
         $htmls = $this->fetch();
 
         // 将数据返回给用户
         return $htmls;
-	}
+    }
+	//从前台获取的term信息，筛选出course_id属性后获取course对象返回给后台
+    public function getCourse() {
+	 	//从前台获取term数组
+        $termId = Request::instance()->param('term/d');
+        $id = session('studentId'); 
+        $Course = Score::where('student_id',$id)->where('term_id',$termId)->column('course_id');
+        $Courseid = array_unique($Course);
+        foreach ($Courseid as $key => $value)
+        {
+         // $value即为term的id  用map这个数组承接
+            $map['id'] = $value;
+         //用键值$key区分term对象  用get()方法获得id，从而获得term对象
+            $courses[$key] = Course::get($map);
+        } 
+        return $courses;
+    }
+	//显示查询出的成绩的界面
+    public function scoreDisplay()
+    {
+		 //从上一个V层获取用户所选择的课程的id，以便获取该课程的学生的信息
+       $courseid = Request::instance()->post('course');  
+         //从上一个V层获取用户所选择的学期的id，以便获取该课程的学生的信息
+       $termid = Request::instance()->post('term');   
+         //获取当前登录学生的id
+       $id = session('studentId');  
+         //获取score表里的全部信息 
+       $score = Score::all();
+         //获取与当前课程id相同的id的课程的全部信息
+       $Courseid = Score::where('student_id',$id)->where('term_id',$termid)->where('course_id',$courseid)->select();
+       $this->assign('Courseids',$Courseid);
+       $htmls = $this->fetch();
+       return $htmls;  
+      // 取回打包后的数据
+       $htmls = $this->fetch();
+
+        // 将数据返回给用户
+       return $htmls;
+   }
 	//留言
-	public function putMessage()
-	{
-		 // 取回打包后的数据
-        $htmls = $this->fetch();
+   public function putMessage()
+   {
+        //获取当前登陆学生的id
+    $id = session('studentId'); 
+        //获取score表里的全部信息
+    $score = Score::all();
+
+        //获取与当前学生id相同的id的学生的全部信息，筛选出course_id字段
+    $Courseid = Score::where('student_id',$id)->column('course_id');
+    
+        //对course_id这个字段进行筛选
+    $CourseId = array_unique($Courseid);
+
+         //根据course_id这个字段进行逐个循环，根据id获取Course这个对象，以便得到course的name
+    foreach ($CourseId as $key => $value)
+    {
+         // $value即为course的id  用map这个数组承接
+        $map['id'] = $value;
+         //用键值$key区分term对象  用get()方法获得id，从而获得course对象
+        $course[$key] = Course::get($map);
+    } 
+           //将获得的对象数组传到v层             
+
+    $this->assign('course',$course);
+         // 取回打包后的数据
+    $htmls = $this->fetch();
 
         // 将数据返回给用户
-        return $htmls;
-	}
-
-	public function forget_password()
-	{
-		return $this->fetch();
-	}
-
-	public function register()
-	{
-		return $this->fetch();
-	}
-
+    return $htmls;
+}
+public function getName() {
+    $CourseId = Request::instance()->param('course/d');
+    $id = session('studentId'); 
+    $Teacher = Score::where('student_id',$id)->where('course_id',$CourseId)->column('teacher_id');
+    $Teacherid = array_unique($Teacher);
+    foreach ($Teacherid as $key => $value)
+    {
+         // $value即为term的id  用map这个数组承接
+        $map['id'] = $value;
+         //用键值$key区分term对象  用get()方法获得id，从而获得term对象
+        $teachers[$key] = Teacher::get($map);
+    } 
+    return $teachers;
+}
+public function sentMessage()
+{
+    $id = session('studentId');
+    $chats = Chat::where('student_id',$id)->select();
+    $this->assign('id',$id);
+    $this->assign('chats',$chats);
+   
+    $teacherId = Request::instance()->param('teacher_id');
+    $this->assign('teacherId',$teacherId);
+    dump($teacherId);
+ // 取回打包后的数据\
+    $htmls = $this->fetch();
+ // 将数据返回给用户
+    return $htmls;
+}
+public function saveMessage()
+{
+    $studentId = session('studentId'); 
+    $teacherId = Request::instance()->param('teacher_id');
+    dump($teacherId);
+    $studentChat = Request::instance()->param('student_chat');
+    $chat = new Chat;
+    if (!is_null($studentId)&& !is_null($teacherId))
+    {
+    $chat->student_id = $studentId;
+    $chat->teacher_id = $teacherId;
+    $chat->student_chat = base64_encode($studentChat);
+    $chat->save();
+    return $this->success('发送成功',url('sentMessage') . '?teacher_id=' . $teacherId);
+}
+}
+public function deleteMessage()
+{
+$teacherId = Request::instance()->param('teacher_id');
+dump($teacherId);
+}
 }
